9958e1ffc002c54074a6d26f7606bc89d72b8dfb
pzworks <piotrziolkowski92@gmail.com>
2021-07-13 23:30:16 +0200

    Added last seen component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import LastSeenComponent from "./LastSeenComponent";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <LastSeenComponent lastSeenTimestamp={Math.floor(Date.now()/1000)} />
     </View>
   );
 }
---
+++ LastSeenComponent.tsx
@@ -0,0 +1,27 @@
+import React, {useEffect, useState} from "react";
+import {Text} from "react-native";
+import {lastSeenProps} from "./types";
+
+const LastSeenComponent = (props: lastSeenProps) => {
+  const { lastSeenTimestamp }: { lastSeenTimestamp: number} = props;
+  const [timeElapsed, setTimeElapsed] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      let now: number = Math.floor(Date.now() / 1000);
+      let timeDiff: number = now - lastSeenTimestamp;
+      if (timeDiff <= 30 || timeDiff % 60 === 0) {
+        setTimeElapsed(now - lastSeenTimestamp);
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  if (timeElapsed > 60) {
+    return <Text>Last seen: {Math.floor(timeElapsed / 60)} minutes ago.</Text>
+  }
+
+  return <Text>Last seen: {timeElapsed} seconds ago.</Text>
+}
+
+export default LastSeenComponent;
