693196f44ad18603bdd25c16cb0387ccd2d7d142
pzworks <piotrziolkowski92@gmail.com>
2021-07-13 23:44:30 +0200

    cleanup

--- LastSeenComponent.tsx
+++ LastSeenComponent.tsx
@@ -3,13 +3,13 @@ import { Text } from "react-native";
 import { lastSeenProps } from "./types";
 
 const LastSeenComponent = (props: lastSeenProps) => {
-  const { lastSeenTimestamp }: { lastSeenTimestamp: number} = props;
+  const { lastSeenTimestamp }: { lastSeenTimestamp: number } = props;
   const [timeElapsed, setTimeElapsed] = useState(0)
 
   useEffect(() => {
     const interval = setInterval(() => {
-      let now: number = Math.floor(Date.now() / 1000);
-      let timeDiff: number = now - lastSeenTimestamp;
+      const now: number = Math.floor(Date.now() / 1000);
+      const timeDiff: number = now - lastSeenTimestamp;
       if (timeDiff <= 30 || timeDiff % 60 === 0) {
         setTimeElapsed(now - lastSeenTimestamp);
       }
